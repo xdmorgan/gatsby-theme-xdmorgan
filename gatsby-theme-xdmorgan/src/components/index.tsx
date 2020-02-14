a1d2a5645686ea4567f9e221aa32c6783172539b
Dan Morgan <hello@danny.codes>
2020-02-13 21:35:12 -0500

    Remove linting and testing, publish 2.0 preview (#7)
    * Remove linting and testing complexity, publish 2.0 preview
    * Confirmed in personal-site, publish 2.0.0

--- gatsby-theme-xdmorgan/src/components/index.tsx
+++
@@ -1,4 +0,0 @@
-// eslint-disable-next-line
-import React from "react";
-// eslint-disable-next-line
-export default ({ name, ...props }) => <div {...props}>Hello, {name}.</div>;
--- gatsby-theme-xdmorgan/src/components/typescript.test.ts
+++
@@ -1,5 +0,0 @@
-import add from "./typescript";
-
-test("testing the tests", () => {
-  expect(add(2, 2)).toEqual(4);
-});
