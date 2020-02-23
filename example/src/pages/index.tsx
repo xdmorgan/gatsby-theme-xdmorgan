1d82e8c6e024e055995272329c0bd102c08cd8c7
Dan Morgan <hello@danny.codes>
2020-02-23 12:15:56 -0500

    Feature/add link component (#10)
    * Export a Link component
    * Update version for release

--- example/src/pages/index.tsx
+++ example/src/pages/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { SEO } from "gatsby-theme-xdmorgan";
 import styles from "./index.module.scss";
+import Header from "../components/header";
 
 export default function Page() {
   return (
     <>
       <SEO title="Home" />
+      <Header />
       <div className={styles.test}>Homepage in a user&rsquo;s site.</div>
     </>
   );
